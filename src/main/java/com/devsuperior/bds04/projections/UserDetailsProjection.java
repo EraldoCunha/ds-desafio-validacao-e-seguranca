2b724b93e58abe1d72352bc720efcf7c59e1e05c
Eraldo Cunha <eraldo_cunha@hotmail.com>
2025-04-10 15:14:43 -0300

    Todos os testes ok

--- src/main/java/com/devsuperior/bds04/projections/UserDetailsProjection.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.bds04.projections;
-
-public interface UserDetailsProjection {
-
-    String getUsername();
-    String getPassword();
-    Long getRoleId();
-    String getAuthority();
-}
